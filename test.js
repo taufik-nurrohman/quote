3fd3da7cad9f6b87619d2b5d77f3782f2bddd6f5
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-11-18 13:53:37 +0700

    Update Test

--- test.js
+++ test.js
@@ -1,12 +1,247 @@
 const test = require('ava');
-const {doubleQuote, singleQuote, HTML} = require('./index.js');
+const {
+    doubleQuote,
+    noQuote,
+    singleQuote,
+    toggleQuote,
+    HTML,
+    JSON,
+    SGML,
+    XML
+} = require('./index.js');
 
-test('common', t => {
-    t.is(doubleQuote("'foo'"), '"foo"');
-    t.is(singleQuote('"foo"'), "'foo'");
+test('doubleQuote', t => {
+    t.is(doubleQuote(`aaa"`), `aaa"`);
+    t.is(doubleQuote(`aaa'`), `aaa"`);
+    t.is(doubleQuote(`aaa\\"`), `aaa\\"`);
+    t.is(doubleQuote(`aaa\\'`), `aaa\\"`);
+    t.is(doubleQuote(`'aaa"`), `"aaa"`);
 });
 
-test('HTML', t => {
-    t.is(HTML.doubleQuote("<span class='foo'>foo</span>"), '<span class="foo">foo</span>');
-    t.is(HTML.singleQuote('<span class="foo">foo</span>'), "<span class='foo'>foo</span>");
+test('noQuote', t => {
+    t.is(noQuote(`aaa"`), `aaa`);
+    t.is(noQuote(`aaa'`), `aaa`);
+    t.is(noQuote(`aaa\\"`), `aaa\\"`);
+    t.is(noQuote(`aaa\\'`), `aaa\\'`);
+    t.is(noQuote(`'aaa"`), `aaa`);
 });
+
+test('singleQuote', t => {
+    t.is(singleQuote(`aaa"`), `aaa'`);
+    t.is(singleQuote(`aaa'`), `aaa'`);
+    t.is(singleQuote(`aaa\\"`), `aaa\\'`);
+    t.is(singleQuote(`aaa\\'`), `aaa\\'`);
+    t.is(singleQuote(`'aaa"`), `'aaa'`);
+});
+
+test('toggleQuote', t => {
+    t.is(toggleQuote(`foo "bar" baz 'qux'`), `foo 'bar' baz "qux"`);
+    t.is(toggleQuote(`foo \\"bar" baz \\'qux'`), `foo \\'bar' baz \\"qux"`);
+});
+
+test('HTML.doubleQuote', t => {
+    t.is(HTML.doubleQuote(`
+<foo bar="baz">
+<foo bar='baz'>
+
+<foo bar="baz 'qux'">
+<foo bar='baz "qux"'>
+
+"foo" bar
+'foo' bar
+
+"foo 'bar'" baz
+'foo "bar"' baz
+
+<foo bar="baz > 0">
+<foo bar="baz < 0">
+
+<foo bar='baz > 0'>
+<foo bar='baz < 0'>
+
+<foo bar="'baz' + qux">
+<foo bar='"baz" + qux'>
+
+<foo bar="'baz=\\"qux\\"' + qux">
+<foo bar='"baz=\\'qux\\'" + qux'>
+
+<scr` + `ipt>
+let node = "<foo bar='baz'>";
+</scr` + `ipt>
+
+<scr` + `ipt foo='bar > 0'>
+let node = "<foo bar='baz'>";
+</scr` + `ipt>
+`), `
+<foo bar="baz">
+<foo bar="baz">
+
+<foo bar="baz 'qux'">
+<foo bar="baz 'qux'">
+
+"foo" bar
+'foo' bar
+
+"foo 'bar'" baz
+'foo "bar"' baz
+
+<foo bar="baz > 0">
+<foo bar="baz < 0">
+
+<foo bar="baz > 0">
+<foo bar="baz < 0">
+
+<foo bar="'baz' + qux">
+<foo bar="'baz' + qux">
+
+<foo bar="'baz=\\"qux\\"' + qux">
+<foo bar="'baz=\\"qux\\"' + qux">
+
+<scr` + `ipt>
+let node = "<foo bar='baz'>";
+</scr` + `ipt>
+
+<scr` + `ipt foo="bar > 0">
+let node = "<foo bar='baz'>";
+</scr` + `ipt>
+`);
+});
+
+test('HTML.noQuote', t => {
+    t.is(HTML.noQuote(`
+<aaa bbb="ccc" ddd eee="fff">
+<aaa bbb='ccc' ddd eee='fff'>
+<aaa bbb=ccc ddd eee=fff>
+
+<aaa bbb="0ccc" ddd eee="0fff">
+<aaa bbb='0ccc' ddd eee='0fff'>
+
+<aaa bbb="ccc ccc" ddd eee="fff fff">
+<aaa bbb='ccc ccc' ddd eee='fff fff'>
+
+<aaa bbb="" ccc ddd="">
+<aaa bbb='' ccc ddd=''>
+`, `
+<aaa bbb=ccc ddd eee=fff>
+<aaa bbb=ccc ddd eee=fff>
+<aaa bbb=ccc ddd eee=fff>
+
+<aaa bbb="0ccc" ddd eee="0fff">
+<aaa bbb='0ccc' ddd eee='0fff'>
+
+<aaa bbb="ccc ccc" ddd eee="fff fff">
+<aaa bbb='ccc ccc' ddd eee='fff fff'>
+
+<aaa bbb= ccc ddd=>
+<aaa bbb= ccc ddd=>
+`);
+});
+
+test('HTML.singleQuote', t => {
+    t.is(HTML.singleQuote(`
+<foo bar="baz">
+<foo bar='baz'>
+
+<foo bar="baz 'qux'">
+<foo bar='baz "qux"'>
+
+"foo" bar
+'foo' bar
+
+"foo 'bar'" baz
+'foo "bar"' baz
+
+<foo bar="baz > 0">
+<foo bar="baz < 0">
+
+<foo bar='baz > 0'>
+<foo bar='baz < 0'>
+
+<foo bar="'baz' + qux">
+<foo bar='"baz" + qux'>
+
+<foo bar="'baz=\\"qux\\"' + qux">
+<foo bar='"baz=\\'qux\\'" + qux'>
+
+<scr` + `ipt>
+let node = '<foo bar="baz">';
+</scr` + `ipt>
+
+<scr` + `ipt foo="bar > 0">
+let node = '<foo bar="baz">';
+</scr` + `ipt>
+`), `
+<foo bar='baz'>
+<foo bar='baz'>
+
+<foo bar='baz "qux"'>
+<foo bar='baz "qux"'>
+
+"foo" bar
+'foo' bar
+
+"foo 'bar'" baz
+'foo "bar"' baz
+
+<foo bar='baz > 0'>
+<foo bar='baz < 0'>
+
+<foo bar='baz > 0'>
+<foo bar='baz < 0'>
+
+<foo bar='"baz" + qux'>
+<foo bar='"baz" + qux'>
+
+<foo bar='"baz=\\'qux\\'" + qux'>
+<foo bar='"baz=\\'qux\\'" + qux'>
+
+<scr` + `ipt>
+let node = '<foo bar="baz">';
+</scr` + `ipt>
+
+<scr` + `ipt foo='bar > 0'>
+let node = '<foo bar="baz">';
+</scr` + `ipt>
+`);
+});
+
+test('JSON.noQuote', t => {
+    t.is(JSON.noQuote(`
+{
+  "foo": "bar",
+  "bar": ["foo", "bar", {
+    "$": 1,
+    "0": 1,
+    "a": 1,
+    "_": 1,
+    "$FooBar": 1,
+    "0FooBar": 1,
+    "aFooBar": 1,
+    "_FooBar": 1
+  }],
+  "baz:qux": 1
+}
+`, `
+{
+  foo: "bar",
+  bar: ["foo", "bar", {
+    $: 1,
+    "0": 1,
+    a: 1,
+    _: 1,
+    $FooBar: 1,
+    "0FooBar": 1,
+    aFooBar: 1,
+    _FooBar: 1
+  }],
+  "baz:qux": 1
+}
+`);
+});
+
+// `SGML` and `XML` are mostly just alias for `HTML`
+test.todo('SGML.doubleQuote');
+test.todo('SGML.noQuote');
+test.todo('SGML.singleQuote');
+test.todo('XML.doubleQuote');
+test.todo('XML.singleQuote');
